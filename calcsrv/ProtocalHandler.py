1fe3459f57d9368ca006f66b80214c539bb0a7cd
Matt Arnold <marnold@telgarwyer.(none)>
2010-04-26 16:18:24 -0400

    ADD calcsrv classes

---
+++ calcsrv/ProtocalHandler.py
@@ -0,0 +1,107 @@
+## Protocal Handler for calcsrv
+## Matt Arnold
+# 4/26/10
+
+from socket import *
+from threading import *
+from config import *
+
+"""
+supported_ops
+
+A dict of all the operations this server supports
+with expected msgs as keys and the python opertor as values
+"""
+
+supported_ops = {"ADD":"+", "SUB":"-","MUL":"*","DIV":"/"}
+
+class ProtocalHandler(Thread):
+
+	def __init__(self, conn, info):
+		
+		self.csock = conn
+		self.cinfo = info
+		self.junk = 0 ## No DoS thank you
+		
+		Thread.__init__(self) ## could use super but we want 2.5 compat
+	
+	def waitfornumber(self):
+	
+		while self.junk < ALLOWED_JUNK:
+			
+			msg = self.csock.recv(BUFFSIZE)
+			proced_msg = msg.strip() # no newlines
+			
+			if proced_msg.isdigit():
+				
+				self.csock.send("OK\n")
+				return proced_msg
+			else:
+				self.csock.send("NCK\n")
+				self.junk += 1
+			# end if-else
+		# end while
+		return 'E'
+	# end def
+	
+	def waitforinit(self):
+		
+		while self.junk < ALLOWED_JUNK:
+			
+			msg = self.csock.recv(BUFFSIZE)
+			proced_msg = msg.strip()
+			
+			if proced_msg == "INIT":
+			
+				self.csock.send("OK\n")
+				return "O"
+			else:
+			
+				self.csock.send("NCK\n")
+				self.junk += 1
+			# end if-else
+		# end while
+		return "E"
+	# end def
+	
+	def waitforop(self):
+		
+		while self.junk < ALLOWED_JUNK:
+		
+			msg = self.csock.recv(BUFFSIZE)
+			proced_msg = msg.strip()
+			
+			if proced_msg in supported_ops:
+				
+				self.csock.send("OK\n")
+				return supported_ops[proced_msg]
+			else:
+				self.csock.send("NCK\n")
+				self.junk += 1
+			# end if-else
+		# end while
+		return "E"
+	#end def
+	
+	def run(self):
+		
+		msg_li['E', 'E', 'E']
+		init = waitforinit(self)
+		if init != "O":
+			self.csock.close()
+			return
+		# end if
+		msg_li[0] = waitfornumber(self)
+		msg_li[2] = waitfornumber(self)
+		msg_li[1] = waitforop(self)
+		
+		if "E" not in msg_li:
+			
+			expr = ''.join(msg_li)
+			finalresp = str(eval(expr)).append("\n")
+			self.csock.send(finalresp)
+		# endif
+		self.csock.close()
+	# enddef
+# end class
+		
---
+++ calcsrv/config.py
@@ -0,0 +1,6 @@
+## config settings for calcsrv
+
+PORT = 1123
+INET_ADDR = '' ## eg bind to all
+ALLOWED_JUNK = 20
+BUFFSIZE = 512 ## Docs say it should be a small power of two
